c4180139bb299165ac68cf385d054a01205bad98
jingge <louiswang0898@gmail.com>
2017-11-29 23:34:33 +0800

    JMeter config

--- src/main/java/gq/jingge/blog/config/WebConfig.java
+++ src/main/java/gq/jingge/blog/config/WebConfig.java
@@ -1,5 +1,6 @@
 package gq.jingge.blog.config;
 
+import gq.jingge.blog.support.ViewHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
