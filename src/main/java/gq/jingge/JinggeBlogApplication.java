7301d1127aa138ca2f63461d1ca43c2aa168ee37
jingge <louiswang0898@gmail.com>
2017-12-14 09:42:17 +0800

    开启缓存策略

--- src/main/java/gq/jingge/JinggeBlogApplication.java
+++ src/main/java/gq/jingge/JinggeBlogApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cache.annotation.EnableCaching;
  * @create 2017-06-12 16:15
  **/
 @SpringBootApplication
-//@EnableCaching
+@EnableCaching
 public class JinggeBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(JinggeBlogApplication.class, args);
