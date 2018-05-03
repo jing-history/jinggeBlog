ad58ba5167451073a9746c8b8ed0baccdd5b11ec
wangyj <wangyj@soonfor.com>
2018-05-03 18:26:20 +0800

    base code

---
+++ src/main/java/gq/jingge/blog/base/offer/chapter2/IntWrapper.java
@@ -0,0 +1,10 @@
+package gq.jingge.blog.base.offer.chapter2;
+
+/**
+ * @author wangyj
+ * @description
+ * @create 2018-05-03 15:59
+ **/
+public class IntWrapper {
+    public int value = 0;
+}
---
+++ src/main/java/gq/jingge/blog/base/offer/chapter2/Result.java
@@ -0,0 +1,17 @@
+package gq.jingge.blog.base.offer.chapter2;
+
+import gq.jingge.blog.base.offer.CtCILibrary.LinkedListNode;
+
+/**
+ * @author wangyj
+ * @description
+ * @create 2018-05-03 16:00
+ **/
+public class Result {
+    public LinkedListNode node;
+    public int count;
+    public Result(LinkedListNode n, int c) {
+        node = n;
+        count = c;
+    }
+}
